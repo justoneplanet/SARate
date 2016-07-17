2817133424419abd7c858e7da537a79a043be807
Mitsuaki Ishimoto <mitsuaki.ishimoto@i-o.tokyo>
2016-07-18 02:16:02 +0900

    remove mainBundle

--- SARate/SARateViewController.m
+++ SARate/SARateViewController.m
@@ -82,9 +82,13 @@
     float starY = 85.0;
     float separatorWidth = 5.0;
     
+    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+    UIImage *starGray = [UIImage imageNamed:@"star-gray.png" inBundle:bundle compatibleWithTraitCollection:nil];
+    UIImage *star = [UIImage imageNamed:@"star.png" inBundle:bundle compatibleWithTraitCollection:nil];
+
     _star1 = [UIButton buttonWithType:UIButtonTypeCustom];
-    [_star1 setImage:[UIImage imageNamed:@"star-gray.png"] forState:UIControlStateNormal];
-    [_star1 setImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateSelected];
+    [_star1 setImage:starGray forState:UIControlStateNormal];
+    [_star1 setImage:star forState:UIControlStateSelected];
     [_star1 addTarget:self action:@selector(setRaiting:) forControlEvents:UIControlEventTouchUpInside];
     _star1.tag = 1;
     _star1.frame = CGRectMake(43, starY, starWeight, starHeight);
@@ -93,8 +97,8 @@
     
     
     _star2 = [UIButton buttonWithType:UIButtonTypeCustom];
-    [_star2 setImage:[UIImage imageNamed:@"star-gray.png"] forState:UIControlStateNormal];
-    [_star2 setImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateSelected];
+    [_star2 setImage:starGray forState:UIControlStateNormal];
+    [_star2 setImage:star forState:UIControlStateSelected];
     [_star2 addTarget:self action:@selector(setRaiting:) forControlEvents:UIControlEventTouchUpInside];
     _star2.tag = 2;
     _star2.frame = CGRectMake(_star1.frame.origin.x+starWeight+separatorWidth, starY, starWeight, starHeight);
@@ -102,8 +106,8 @@
     
     
     _star3 = [UIButton buttonWithType:UIButtonTypeCustom];
-    [_star3 setImage:[UIImage imageNamed:@"star-gray.png"] forState:UIControlStateNormal];
-    [_star3 setImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateSelected];
+    [_star3 setImage:starGray forState:UIControlStateNormal];
+    [_star3 setImage:star forState:UIControlStateSelected];
     [_star3 addTarget:self action:@selector(setRaiting:) forControlEvents:UIControlEventTouchUpInside];
     _star3.tag = 3;
     _star3.frame = CGRectMake(_star2.frame.origin.x+starWeight+separatorWidth, starY, starWeight, starHeight);
@@ -111,8 +115,8 @@
     
     
     _star4 = [UIButton buttonWithType:UIButtonTypeCustom];
-    [_star4 setImage:[UIImage imageNamed:@"star-gray.png"] forState:UIControlStateNormal];
-    [_star4 setImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateSelected];
+    [_star4 setImage:starGray forState:UIControlStateNormal];
+    [_star4 setImage:star forState:UIControlStateSelected];
     [_star4 addTarget:self action:@selector(setRaiting:) forControlEvents:UIControlEventTouchUpInside];
     _star4.tag = 4;
     _star4.frame = CGRectMake(_star3.frame.origin.x+starWeight+separatorWidth, starY, starWeight, starHeight);
@@ -120,8 +124,8 @@
     
     
     _star5 = [UIButton buttonWithType:UIButtonTypeCustom];
-    [_star5 setImage:[UIImage imageNamed:@"star-gray.png"] forState:UIControlStateNormal];
-    [_star5 setImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateSelected];
+    [_star5 setImage:starGray forState:UIControlStateNormal];
+    [_star5 setImage:star forState:UIControlStateSelected];
     [_star5 addTarget:self action:@selector(setRaiting:) forControlEvents:UIControlEventTouchUpInside];
     _star5.tag = 5;
     _star5.frame = CGRectMake(_star4.frame.origin.x+starWeight+separatorWidth, starY, starWeight, starHeight);
